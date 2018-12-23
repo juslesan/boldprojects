a63993072682d71747f522125fc1e52bca789cc2
Juslesan <santeri.juslenius@gmail.com>
2018-12-23 23:25:59 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import logo from './img/logo.png';
 
-class App extends Component {
+// import react-bootstrap from 'react-bootstrap'
+class App extends React.Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+          
+      }
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      return (
+        <div className="container" style={{ position: 'relative', marginLeft: '20%', marginTop:'30px', marginRight: '20%'}}>
+            <div className="front" >
+                <img src={logo} alt='logo' width='450px' style={{marginLeft: '13%'}}></img>
+                <p>asdasdasdasd</p>
+            </div>
+        </div>
+    )
   }
 }
 
