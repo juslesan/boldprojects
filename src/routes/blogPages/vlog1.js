2e570a54db776c5594faf6adebfa45dadd112c1b
Juslesan <santeri.juslenius@gmail.com>
2019-03-01 17:18:04 +0200

    youtube

--- src/routes/blogPages/vlog1.js
+++ src/routes/blogPages/vlog1.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import YouTube from 'react-youtube';
+import './vlog.css'
 
 const Vlog1 = () => {
     return (
-        <div></div>
+        <div className="vlog">
+            <YouTube
+                videoId="vQKf-IFN4DU"
+                className="embedded"
+            />
+        </div>
     )
 }
 
